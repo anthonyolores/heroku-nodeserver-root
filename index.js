cb500dd3642953234c5169dfa76bfb740d9d8508
Anthony Olores <anthony@ABL.local>
2019-03-14 18:29:35 +1300

    updated index.js

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var express = require('express')
 var app = express()
 
+const http = require('http');
+const port=process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
+
+server.listen(port,() => {
+console.log(`Server running at port `+port);
+});
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
   res.send('hello world')
